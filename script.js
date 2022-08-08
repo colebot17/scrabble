bf9a473b0f4e50caf5a2cdd5869a3772304c825b
Colebot17 <colebot17@gmail.com>
2022-08-08 19:35:14 -0400

    Use relative references for all ajax requests

--- script.js
+++ script.js
@@ -444,7 +444,7 @@ function createGame(playerList = JSON.parse(document.getElementById('createGameP
 	}
 
 	$.ajax(
-		'http://scrabble.colebot.com/newGame.php',
+		'newGame.php',
 		{
 			data: {user: account.id, pwd: account.pwd, players: JSON.stringify(players)},
 			method: "POST",
@@ -486,7 +486,7 @@ function loadGame(id = prompt("Enter the id of the game you want to load:"), exp
 			setTimeout(function() {clone.remove()}, 740);
 		}
 		$.ajax(
-			'http://scrabble.colebot.com/loadGame.php',
+			'loadGame.php',
 			{
 				data: {user: account.id, pwd: account.pwd, game: id},
 				method: "POST",
