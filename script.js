7ccfad1e6270cc1ba38834a190987b95e9e24c43
Colebot17 <colebot17@gmail.com>
2022-08-08 21:06:59 -0400

    Move chat code into separate js file

--- script.js
+++ script.js
@@ -1006,68 +1006,6 @@ function gameInit() {
 	chatInit();
 }
 
-function chatInit() {
-	const chat = game.chat;
-	const chatContentBox = $('.chatContent').empty();
-
-	let chatContent = ``;
-
-	const currentDate = new Date();
-
-	for (let i in chat) {
-		// determine what to show in the timestamp field
-		const messageDate = new Date(chat[i].timestamp);
-
-		const isToday = currentDate.toDateString() === messageDate.toDateString();
-		const isYesterday = new Date().setDate(currentDate.getDate() - 1) === messageDate.toDateString();
-		const sameWeek = new Date().setDate(currentDate.getDate() - 7) > messageDate;
-
-		let dateString;
-
-		if (isToday) {
-			// display time
-			const rawHours = messageDate.getHours();
-			const rawMinutes = messageDate.getMinutes();
-
-			const hours = (rawHours % 12) + (rawHours % 12 === 0 ? 12 : 0);
-			const minutes = (rawMinutes.toString().length === 1 ? "0" : "") + rawMinutes;
-			const period = (rawHours > 11 ? "PM" : "AM");
-
-			dateString = `${hours}:${minutes} ${period}`;
-		} else if (isYesterday) {
-			dateString = "Yesterday";
-		} else if (sameWeek) {
-			// show day of week
-			const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-			dateString = daysOfWeek[messageDate.getDay()];
-		} else {
-			// show month abbrv. and day of month
-			const monthAbbrvs = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-			const dayOfMonth = (messageDate.getDate().toString().length === 1 ? "0" : "") + messageDate.getDate() + 1;
-
-			dateString = `${monthAbbrvs[messageDate.getMonth()]} ${dayOfMonth}`;
-		}
-
-		chatContent += `
-			<div class="chatMessage">
-				<div class="chatMessageLine1">
-					<div class="chatMessageSender">
-						${chat[i].senderName}
-					</div>
-					<div class="chatMessageTimestamp">
-						${dateString}
-					</div>
-				</div>
-				<div class="chatMessageText">
-					${chat[i].message}
-				</div>
-			</div>
-		`;
-	}
-
-	chatContentBox.html(chatContent);
-}
-
 function dictLookup(words, callback = function(entries) {}) {
 	let entries = [];
 	let promises = [
