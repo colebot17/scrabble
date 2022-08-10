1b8b48504fdc94faf34e597d5a79b8065a7237db
Colebot17 <colebot17@gmail.com>
2022-08-09 20:31:58 -0400

    Change empty chat message

--- chat.js
+++ chat.js
@@ -60,7 +60,7 @@ function chatInit() {
 		`;
 	}
 
-	chatContentBox.html(chatContent || "There are no messages in this chat. You can be the first!").css('align-items', (chatContent ? '' : 'center'));
+	chatContentBox.html(chatContent || "This chat is empty. :(").css('align-items', (chatContent ? '' : 'center'));
     chatInput.val('');
 
     chatContentBox[0].scrollTop = chatContentBox[0].scrollHeight;
