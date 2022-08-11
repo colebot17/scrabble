69bdabb91d0aa310a886d48902b421bd58d7e464
Colebot17 <colebot17@gmail.com>
2022-08-11 13:15:28 -0400

    Organize create account code

--- createAccount.php
+++ createAccount.php
@@ -16,29 +16,35 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
+$name = trim($name); // trim the name
+
 // validate name and password
 $nameValid = strlen($name) >= 2; // name must be at least two characters
 $pwdValid = strlen($pwd) >= 8; // pwd must be at least eight characters
 
-if ($nameValid && $pwdValid) {
-	// check if account by name already exists
-	$sql = "SELECT id FROM accounts WHERE name='$name'";
-	$query = mysqli_query($conn, $sql);
-	if (!mysqli_fetch_assoc($query)) {
-		// hash the password
-		$hash = password_hash($pwd, PASSWORD_DEFAULT);
-
-		// create the account
-		$sql = "INSERT INTO accounts(name, pwd, games) VALUES ('$name', '$hash', '[]
-			');";
-		$query = mysqli_query($conn, $sql);
-		echo $name;
-	} else {
-		echo "0";
-	}	
-} else {
-	echo "0";
+// require a valid name and password
+if (!$nameValid || !$pwdValid) {
+	exit('0');
 }
+
+// make sure username is unique
+$sql = "SELECT id FROM accounts WHERE name='$name'";
+$query = mysqli_query($conn, $sql);
+if (mysqli_fetch_assoc($query)) {
+	exit('0');
+}
+
+// escape the username
+$name = str_replace("'", "\'", $name);
+$name = str_replace('"', '\"', $name);
+
+// hash the password
+$hash = password_hash($pwd, PASSWORD_DEFAULT);
+
+// create the account
+$sql = "INSERT INTO accounts(name, pwd, games) VALUES ('$name', '$hash', '[]');";
+$query = mysqli_query($conn, $sql);
+echo $name;
 
 // close the connection
 $conn->close();
