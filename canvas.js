b94e1127832b07d213122253364d750d7939aa15
Colebot17 <colebot17@gmail.com>
2022-08-15 19:36:46 -0400

    Fix typo

--- canvas.js
+++ canvas.js
@@ -128,11 +128,11 @@ function drawLetterBank() {
 	canvas.ctx.fillText("Letter Bank", canvasWidth / 2, startY + titleSize + 10);
 
 	// draw the shuffle bank button
-	canvas.ctx.font = titleSize + "px Rubik";
+	canvas.ctx.font = titleSize + "px Material Icons";
 	canvas.ctx.fillStyle = "#000000";
 	canvas.ctx.textAlign = "center";
 	
-	const shuffleButtonX = (canvas.width / 2) + 75;
+	const shuffleButtonX = (canvasWidth / 2) + 75;
 	const shuffleButtonY = startY + titleSize + 10;
 	canvas.ctx.fillText("shuffle", shuffleButtonX, shuffleButtonY);
 
