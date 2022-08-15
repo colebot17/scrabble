42ff9e5ad0ce9051ed9e4ce7533daae765ceab33
Colebot17 <colebot17@gmail.com>
2022-08-15 10:29:21 -0400

    Use order index in drop zones instead of bank index

--- script.js
+++ script.js
@@ -927,7 +927,7 @@ function gameInit() {
 							if (i == 0) {
 								canvas.extraGapBeforeBank = true;
 							} else {
-								canvas.bank[canvas.bankOrder[canvas.dropZones[i].bankIndex - 1]].extraGapAfter = true;
+								canvas.bank[canvas.bankOrder[canvas.dropZones[i].orderIndex - 1]].extraGapAfter = true;
 							}
 						}
 					}
