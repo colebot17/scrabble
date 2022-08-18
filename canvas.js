14f93daa75e0da39d8e374bf85fb043d02ee3f58
colebot17 <colebot17@gmail.com>
2022-08-18 11:28:42 -0400

    Fix all letters offset to left

--- canvas.js
+++ canvas.js
@@ -245,7 +245,7 @@ function drawLetterBank() {
 
 			const frame = canvas.animations.bankShuffle.getFrame();
 			const frameMultiplier = Math.abs(frame - 0.5) * 2;
-			x = ((x - (canvasWidth / 2) - (tileWidth / 2)) * frameMultiplier) + (canvasWidth / 2);
+			x = ((x - (canvasWidth / 2) - (tileWidth / 2)) * frameMultiplier) + (canvasWidth / 2) + (tileWidth / 2);
 			y += Math.sin(
 				(frameMultiplier - 1)
 				* Math.PI
