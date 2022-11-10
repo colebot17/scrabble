e3c4867c314195abbad5c9420586146123952fea
colebot17 <colebot17@gmail.com>
2022-11-09 21:21:29 -0500

    Move fullscreen code into separate file

---
+++ scripts/fullscreen.js
@@ -0,0 +1,25 @@
+function fullScreen() {
+	let requestMethod = document.documentElement.requestFullscreen ||
+						document.documentElement.webkitRequestFullscreen ||
+						document.documentElement.webkitRequestFullScreen;
+	if (requestMethod) {
+		requestMethod.apply(document.documentElement).then(() => {document.getElementById('fullscreenIcon').innerHTML = 'fullscreen_exit';});
+	}
+}
+
+function fullScreenExit() {
+	let requestMethod = document.exitFullscreen ||
+						document.webkitExitFullscreen ||
+						document.webkitExitFullScreen;
+	if (requestMethod) {
+		requestMethod.apply(document).then(() => {document.getElementById('fullscreenIcon').innerHTML = 'fullscreen';});
+	}
+}
+
+function toggleFullScreen() {
+	if (!document.fullscreenElement) {
+		fullScreen();
+	} else if (document.exitFullscreen || document.webkitExitFullscreen) {
+		fullScreenExit();
+	}
+}
--- scripts/script.js
+++ scripts/script.js
@@ -564,32 +564,6 @@ function reloadGame() {
 	}
 }
 
-function fullScreen() {
-	let requestMethod = document.documentElement.requestFullscreen ||
-						document.documentElement.webkitRequestFullscreen ||
-						document.documentElement.webkitRequestFullScreen;
-	if (requestMethod) {
-		requestMethod.apply(document.documentElement).then(() => {document.getElementById('fullscreenIcon').innerHTML = 'fullscreen_exit';});
-	}
-}
-
-function fullScreenExit() {
-	let requestMethod = document.exitFullscreen ||
-						document.webkitExitFullscreen ||
-						document.webkitExitFullScreen;
-	if (requestMethod) {
-		requestMethod.apply(document).then(() => {document.getElementById('fullscreenIcon').innerHTML = 'fullscreen';});
-	}
-}
-
-function toggleFullScreen() {
-	if (!document.fullscreenElement) {
-		fullScreen();
-	} else if (document.exitFullscreen || document.webkitExitFullscreen) {
-		fullScreenExit();
-	}
-}
-
 // remove the game
 function endGame() {
 	// make sure user is signed in and in a game and whatever
