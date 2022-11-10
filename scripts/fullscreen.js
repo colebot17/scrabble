8757d0cb3c070e027a713272428de497e3974732
colebot17 <colebot17@gmail.com>
2022-11-10 13:32:47 -0500

    Add test statements

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -2,6 +2,7 @@ function fullScreen() {
 	let requestMethod = document.documentElement.requestFullscreen ||
 						document.documentElement.webkitRequestFullscreen ||
 						document.documentElement.webkitRequestFullScreen;
+	console.log(requestMethod);
 	if (requestMethod) {
 		requestMethod.apply(document.documentElement)//.then(() => {document.getElementById('fullscreenIcon').innerHTML = 'fullscreen_exit';});
 	}
@@ -11,12 +12,14 @@ function fullScreenExit() {
 	let requestMethod = document.exitFullscreen ||
 						document.webkitExitFullscreen ||
 						document.webkitExitFullScreen;
+	console.log(requestMethod);
 	if (requestMethod) {
 		requestMethod.apply(document)//.then(() => {document.getElementById('fullscreenIcon').innerHTML = 'fullscreen';});
 	}
 }
 
 function toggleFullScreen() {
+	console.log(document.fullScreenElement);
 	if (!document.fullscreenElement) {
 		fullScreen();
 	} else if (document.exitFullscreen || document.webkitExitFullscreen) {
