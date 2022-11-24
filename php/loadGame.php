065721e271dd74655cb4fc4f50476719527cd28e
Colebot17 <colebot17@gmail.com>
2022-11-24 16:24:23 -0500

    Get creation date from server when loading game

--- php/loadGame.php
+++ php/loadGame.php
@@ -22,7 +22,7 @@ $sql = "SELECT pwd, games FROM accounts WHERE id='$user'";
 $query = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($query);
 if (password_verify($userPwd, $row['pwd']) && in_array($gameId, json_decode($row['games'], true))) {
-	$sql = "SELECT name, letterBag, players, turn, inactive, board, chat FROM games WHERE id='$gameId'";
+	$sql = "SELECT name, letterBag, players, turn, inactive, board, creationDate, chat FROM games WHERE id='$gameId'";
 	$query = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_assoc($query);
 
