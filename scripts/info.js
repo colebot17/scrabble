ab598c7be3a49928c7d205938b9cd8169a1bfc3f
Colebot17 <colebot17@gmail.com>
2022-11-25 21:35:40 -0500

    Fix typo

--- scripts/info.js
+++ scripts/info.js
@@ -17,7 +17,7 @@ function getInfo() {
             <div class="flex col" style="gap: 5px">
                 ${name ? /* html */ `<div>Name: <b>${name}</b></div>` : ``}
                 <div>Id: <b>${id}</b></div>
-                <div>Created on ${creationDate !== '0000-00-00' ? /* html */ `on <b>${creationDate}</b> ` : ``}by <b>${startPlayer}</b></div>
+                <div>Created ${creationDate !== '0000-00-00' ? /* html */ `on <b>${creationDate}</b> ` : ``}by <b>${startPlayer}</b></div>
                 <div>Turn <b>${totalTurn}</b>: <b>${turnPlayer}</b>'s turn</div>
                 <div><b>${lettersLeft}</b> letters left in bag</div>
             </div>
