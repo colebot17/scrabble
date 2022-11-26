c7f1eabde278cea3882e139247c0757c277678e5
Colebot17 <colebot17@gmail.com>
2022-11-25 21:19:03 -0500

    Add end date and total time to info

--- scripts/info.js
+++ scripts/info.js
@@ -1,25 +1,18 @@
 function getInfo() {
-
+    // common info calculations
     const name = game.name;
     const id = game.id;
     const creationDate = game.creationDate;
     const startPlayer = game.players[0].name;
-    const turnPlayer = game.players[game.turn % game.players.length].name;
-    let winner = "No one", winnerPoints = 0;
-    for (let i in game.players) {
-        if (game.players[i].points > winnerPoints) {
-            winnerPoints = game.players[i].points;
-            winner = game.players[i].name;
-        } else if (game.players[i].points === winnerPoints) {
-            winner += ' and ' + game.players[i].name;
-        }
-    }
     const totalTurn = game.turn;
-    const lettersLeft = game.lettersLeft;
 
     let message;
     if (!game.inactive) {
-        // active game info
+        // active info calculations
+        const lettersLeft = game.lettersLeft;
+        const turnPlayer = game.players[game.turn % game.players.length].name;
+
+        // active info message
         message = /* html */ `
             <div class="flex col" style="gap: 5px">
                 ${name ? /* html */ `<div>Name: <b>${name}</b></div>` : ``}
@@ -30,13 +23,27 @@ function getInfo() {
             </div>
         `;
     } else {
-        // inactive game info
+        // inactive info calculations
+        let winner = "No one", winnerPoints = 0;
+        for (let i in game.players) {
+            if (game.players[i].points > winnerPoints) {
+                winnerPoints = game.players[i].points;
+                winner = game.players[i].name;
+            } else if (game.players[i].points === winnerPoints) {
+                winner += ' and ' + game.players[i].name;
+            }
+        }
+        const endDate = game.endDate;
+        const days = (new Date(game.endDate).now() - new Date(game.creationDate).now()) / 86_400_000; // number of milliseconds in a day
+
+        // inactive info message
         message = /* html */ `
             <div class="flex col" style="gap: 5px">
                 ${name ? /* html */ `<div>Name: <b>${name}</b></div>` : ``}
                 <div>Id: <b>${id}</b></div>
                 <div>Created on <b>${creationDate}</b> by <b>${startPlayer}</b></div>
-                <div><b>${winner}</b> won with <b>${winnerPoints}</b> points in <b>${totalTurn}</b> total moves</div>
+                <div><b>${winner}</b> won with <b>${winnerPoints}</b> points</div>
+                <div>Ended on <b>${endDate}</b> in <b>${days}</b> days with <b>${totalTurn}</b> moves</div>
             </div>
         `
     }
