72a7147384e3c83b9282eadc2cf97c3a76c4afda
Colebot17 <colebot17@gmail.com>
2022-12-04 12:38:59 -0500

    Add php to change password

---
+++ php/changePassword.php
@@ -0,0 +1,47 @@
+<?php
+
+// define connection
+$servername = "p3nlmysql21plsk.secureserver.net:3306";
+$username = "Colebot";
+$password = "96819822";
+$dbname = "scrabble";
+
+// get data from GET/POST
+$user = $_POST['user'];
+$pwd = $_POST['pwd'];
+$newPwd = $_POST['newPwd'];
+
+// create and check connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+// check current password
+$sql = "SELECT pwd FROM accounts WHERE id='$user'";
+$query = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($query);
+if (!password_verify($pwd, $row['pwd'])) {
+	exit('{"errorLevel":2,"message":"Invalid Session!"}');
+}
+
+// validate password
+$pwdValid = strlen($newPwd) >= 8; // pwd must be at least eight characters
+
+// require a valid password
+if (!$pwdValid) {
+	exit('{"errorLevel":1,"message":"Password must consist of at least eight characters."}');
+}
+
+// hash the password
+$hash = password_hash($newPwd, PASSWORD_DEFAULT);
+
+// change the password
+$sql = "UPDATE accounts SET pwd='$hash' WHERE id='$user'";
+$query = mysqli_query($conn, $sql);
+echo '{"errorLevel":0,"message":"Password changed."}';
+
+// close the connection
+$conn->close();
+
+?>
