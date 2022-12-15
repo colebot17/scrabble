9e958b0ee864b7b212952091950bdec3ae688093
Colebot17 <colebot17@gmail.com>
2022-12-14 21:54:32 -0500

    Don't draw 'null' on blank tiles being dragged

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -344,7 +344,7 @@ function updateTile(tile) {
 	}
 	canvas.ctx.font = fontSize + "px Eurostile";
 	canvas.ctx.textAlign = "center";
-	canvas.ctx.fillText(tile.letter, pixelX + (tileWidth / 2), pixelY + fontSize);
+	canvas.ctx.fillText(tile.letter || "", pixelX + (tileWidth / 2), pixelY + fontSize);
 
 	// draw the points on the tile if size allows
 	if (squareWidth >= 35 && !tile.blank) {
@@ -355,6 +355,7 @@ function updateTile(tile) {
 	}
 }
 
+// draw loop
 // this function is run to draw each frame
 function updateDisplay() {
 	clearCanvas();
