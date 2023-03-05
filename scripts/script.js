38ad87acee93d4753758bfb7e9cc6dbc1abecc49
colebot17 <colebot17@gmail.com>
2023-03-05 11:44:45 -0600

    Show a banner when out of turn

--- scripts/script.js
+++ scripts/script.js
@@ -741,6 +741,10 @@ function gameInit() {
 			e.preventDefault();
 			textModal((game.inactive ? "Inactive Game" : "Not your turn!"), (game.inactive ? "This game is inactive, meaning it can no longer be played. You can still look at it all you want, though." : "It's someone else's turn right now. Wait for your turn to make a move.")); // show an alert when the user tries to interact with the canvas or letter bank
 		});
+
+		const banner = document.getElementById('#gameBanner')
+		banner.innerHTML = "It isn't your turn. You can still view the game and rearrange your letters for now.";
+		banner.classList.remove('hidden');
 	}
 
 	// show the game info
