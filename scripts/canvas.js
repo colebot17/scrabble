74a39b9c0a967089a81c3cf941a0f5badc1cf043
colebot17 <colebot17@gmail.com>
2023-03-07 22:28:56 -0500

    Adjust position of multiplier strings

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -117,7 +117,10 @@ function drawBoard() {
 				canvas.ctx.font = fontSize + "px Rubik";
 				canvas.ctx.fillStyle = "#f2f5ff";
 				canvas.ctx.textAlign = "center";
-				canvas.ctx.fillText(boardMultiplierStrings[boardModifiers[y][x]], (x * squareWidth) + (x * squareGap) + (squareWidth / 2), (y * squareWidth) + (y * squareGap) + (squareWidth / 2) + (fontSize / 2));
+				canvas.ctx.textBaseline = "middle";
+				canvas.ctx.fillText(boardMultiplierStrings[boardModifiers[y][x]], (x * squareWidth) + (x * squareGap) + (squareWidth / 2), (y * squareWidth) + (y * squareGap) + (squareWidth / 2));
+
+				canvas.ctx.textBaseline = "";
 			}
 		}
 	}
