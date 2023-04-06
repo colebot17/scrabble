19ea2196e99236b02d7ea4ce639c5c004f7485d5
colebot17 <colebot17@gmail.com>
2023-04-05 22:45:41 -0400

    Fix unwanted gaps when clearing board

--- scripts/events.js
+++ scripts/events.js
@@ -14,8 +14,10 @@ function handleCanvasDblClick(e) { // EVENT OBJECT MAY NOT BE AVAILABLE
     // un-hide all letters in bank
     for (let i in canvas.bank) {
         canvas.bank[i].hidden = false;
+        canvas.bank[i].gapAnimation = undefined;
         canvas.bank[i].extraGapAfter = 0;
     }
+    canvas.gapBeforeBankAnimation = undefined;
     canvas.extraGapBeforeBank = 0;
 
     // remove points preview
