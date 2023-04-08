04a787f334d747c9415d8d6207bf7c47ed5b6cb0
colebot17 <colebot17@gmail.com>
2023-04-08 13:12:53 -0400

    Remove charset header

--- scripts/util.js
+++ scripts/util.js
@@ -44,7 +44,7 @@ function request(filename, dataObj) {
 
 		let xhr = new XMLHttpRequest();
 		xhr.open('POST', url, true);
-		xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+		xhr.setRequestHeader('Content-type', 'application/json')
 		xhr.send(data);
 
 		xhr.onload = function() {
