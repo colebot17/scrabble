df70d50228c0eab7c97b3083085ac29e6012fd52
colebot17 <colebot17@gmail.com>
2023-04-10 13:17:42 -0400

    math.ceil

--- scripts/chat.js
+++ scripts/chat.js
@@ -104,7 +104,7 @@ function chatInit(dontClearInput = false, dontScroll = false) {
 
 function chatScrollBottom() {
 	const chatContentBox = document.getElementsByClassName('chatContent')[0];
-	chatContentBox.scrollTop = chatContentBox.scrollHeight - chatContentBox.getBoundingClientRect().height;
+	chatContentBox.scrollTop = Math.ceil(chatContentBox.scrollHeight - chatContentBox.getBoundingClientRect().height);
 	if (document.getElementsByClassName('chatUpdatePopup')[0]) hideChatUpdatePopup();
 }
 
