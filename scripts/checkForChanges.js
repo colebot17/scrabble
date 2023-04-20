a2c06f9c63a92de537a93317c1e2229de2a9bddb
colebot17 <colebot17@gmail.com>
2023-04-20 11:22:04 -0400

    Fix typo

--- scripts/checkForChanges.js
+++ scripts/checkForChanges.js
@@ -24,12 +24,12 @@ function checkForChanges() {
         if (res.data.length > 0) {
             update(res.data);
         }
-        startChangeCheck();
+        nextChangeCheck();
     }).catch((error) => {
         console.error(error);
         textModal('Error', 'An error occurred checking for changes. Try again?', {
             cancelable: true,
-            complete: startChangeCheck
+            complete: nextChangeCheck
         });
     });
 }
