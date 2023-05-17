8d79442070c073f1819db6625ff3c30157ce510c
colebot17 <colebot17@gmail.com>
2023-05-17 14:45:26 -0400

    Add function to load friends list

--- scripts/friends.js
+++ scripts/friends.js
@@ -1,3 +1,51 @@
 function setFriendsPage(page) {
     document.getElementById('friendsCell').dataset.page = page;
 }
+
+function updateFriendsList(friends) {
+    const friendsList = document.getElementById('friendsList');
+
+    friendsList.innerHTML = "";
+
+    // add each friend
+    let listContents = ``;
+    for (let i = 0; i < friends.length; i++) {
+        const friend = friends[i];
+        let listItem = `
+            <div class="friendListFriend" id="friend${i}" data-friendid="${friend.id}">
+                <button class="friendCheckbox iconButton" onclick="toggleFriendCheckbox(${i})>
+                    <span class="material-symbols-rounded">
+                        check_box_outline_blank
+                    </span>
+                </button>
+                <div class="friendNameContainer flex col">
+                    <span class="friendName">
+                        ${friend.name}
+                    </span>
+                    <span class="finePrint friendInfo">
+                        ${friend.numGames} game${friend.numGames !== 1 ? `s` : ``}
+                    </span>
+                </div>
+                <div class="friendControls">
+                    <button id="removeFriendButton" class="iconButton" title="Remove Friend" onclick="removeFriend(${friend.id})">
+                        <span class="material-symbols-rounded">
+                            person_remove
+                        </span>
+                    </button>
+                </div>
+            </div>
+        `;
+    }
+}
+
+function toggleFriendCheckbox(friendIndex) {
+    const checked = 'check_box';
+    const unchecked = 'check_box_outline_blank';
+
+    const icon = document.querySelector('#friend' + friendIndex + ' .friendCheckbox span');
+
+    const isChecked = icon.innerHTML === checked;
+    icon.innerHTML = isChecked ? unchecked : checked;
+
+    return !isChecked;
+}
