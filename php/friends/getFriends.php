5cc1fab1175c6197a8f03bffb9b95c29d5b6dd99
colebot17 <colebot17@gmail.com>
2023-06-02 12:16:55 -0400

    Separate out variable

--- php/friends/getFriends.php
+++ php/friends/getFriends.php
@@ -22,7 +22,8 @@ function getFriends($conn, $userId) {
         // only count the games that are active
         $numSharedGamesActive = 0;
         for ($j = 0; $j < count($sharedGames); $j++) {
-            $sql = "SELECT inactive FROM games WHERE id='$sharedGames[$j]'";
+            $currentId = $sharedGames[$i];
+            $sql = "SELECT inactive FROM games WHERE id='$currentId'";
             $query = mysqli_query($conn, $sql);
             $row = mysqli_fetch_assoc($query);
             if ($row['inactive'] === 0) {
