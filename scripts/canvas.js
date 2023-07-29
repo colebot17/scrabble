5a61e94f3d448db2fa6dcdd247e50354ef369427
colebot17 <colebot17@gmail.com>
2023-07-29 10:30:27 -0400

    Fix operations

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -411,7 +411,7 @@ function updateTile(tile) {
 
 	// TILE BROWN LIGHT "#a47449cc"
 	const distFromCenter = Math.sqrt(Math.pow(tile.x - 7, 2) + Math.pow(tile.y - 7, 2));
-	canvas.ctx.fillStyle = (tile.turn ?  "hsl(" + ((360 * 10) / distFromCenter) + ", 100%, 50%)": "#a47449"); // tile brown
+	canvas.ctx.fillStyle = (tile.turn ?  "hsl(" + ((360 / 10) * distFromCenter) + ", 100%, 50%)": "#a47449"); // tile brown
 	roundRect(canvas.ctx, pixelX, pixelY, tileWidth, tileWidth, borderRadius);
 
 	// draw the letter on the tile
