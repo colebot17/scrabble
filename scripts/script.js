589e27fa7ff0f9065dcab1db1dd794f4d4065466
colebot17 <colebot17@gmail.com>
2023-07-30 19:03:12 -0400

    Move winnerstring function

--- scripts/script.js
+++ scripts/script.js
@@ -84,6 +84,33 @@ function loadGamesList(done) {
 	}
 }
 
+function winnerString(winners) {
+	let winnerString = "";
+	if (winners.length === 1) {
+		if (winners[0].id === account.id) {
+			winnerString = "You";
+		} else {
+			winnerString = /* html */ `<b>${winners[0].name}</b>`;
+		}
+	} else if (winners.length === 2) {
+		if (winners[0].id === account.id || winners[1].id === account.id) {
+			winnerString = "You and ";
+			winnerString += `<b>${winners.find(a => a.id !== account.id).name}</b>`;
+		} else {
+			winnerString = /* html */ `<b>${winners[0].name}</b> and <b>${winners[1].name}</b>`;
+		}
+	} else if (winners.length >= 3) {
+		for (let j = 0; j < winners.length; j++) {
+			if (j < winners.length - 1) {
+				winnerString += /* html */ `<b>${winners[j].name}</b>, `;
+			} else {
+				winnerString += /* html */ `and <b>${winners[j].name}</b>`;
+			}
+		}
+	}
+	return winnerString;
+}
+
 function updateGamesList() {
 	if (!account.games) return;
 
@@ -388,33 +415,6 @@ function updateGamesList() {
 	// 		loadGamesList(done);
 	// 	}
 	// });
-}
-
-function winnerString(winners) {
-	let winnerString = "";
-	if (winners.length === 1) {
-		if (winners[0].id === account.id) {
-			winnerString = "You";
-		} else {
-			winnerString = /* html */ `<b>${winners[0].name}</b>`;
-		}
-	} else if (winners.length === 2) {
-		if (winners[0].id === account.id || winners[1].id === account.id) {
-			winnerString = "You and ";
-			winnerString += `<b>${winners.find(a => a.id !== account.id).name}</b>`;
-		} else {
-			winnerString = /* html */ `<b>${winners[0].name}</b> and <b>${winners[1].name}</b>`;
-		}
-	} else if (winners.length >= 3) {
-		for (let j = 0; j < winners.length; j++) {
-			if (j < winners.length - 1) {
-				winnerString += /* html */ `<b>${winners[j].name}</b>, `;
-			} else {
-				winnerString += /* html */ `and <b>${winners[j].name}</b>`;
-			}
-		}
-	}
-	return winnerString;
 }
 
 function setDisplayMode(mode) {
