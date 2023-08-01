2eca97d727eaaa0543e24fe2700e4f1928c24044
colebot17 <colebot17@gmail.com>
2023-08-01 14:24:52 -0400

    Add a tutorial

---
+++ scripts/tutorial.js
@@ -0,0 +1,89 @@
+// https://codepen.io/colebot17/pen/jOQXaZj
+
+class TutorialStep {
+    constructor (selector, content, preFn = () => {}, postFn = () => {}) {
+        this.selector = selector;
+        this.content = content;
+        this.preFn = preFn;
+        this.postFn = postFn;
+    }
+}
+
+const scrabbleTutorial = [
+    new TutorialStep('#gamesListRefresh', "Press this to get the latest games list!", () => {console.log("pre")}, () => {console.log("post")})
+];
+
+function startTutorial(tutorial = scrabbleTutorial, startingAt = 0) {
+    const step = tutorial[startingAt];
+    const nextStep = tutorial[startingAt + 1];
+
+    step.preFn();
+
+    const pointingAt = document.querySelector(step.selector);
+    showOverlay(pointingAt, step.content, () => {
+        step.postFn();
+        if (nextStep) {
+            startTutorial(tutorial, startingAt + 1);
+        } else {
+            hideOverlay();
+        }
+    });
+}
+
+function showOverlay(element, text, next = hideOverlay) {
+    const overlay = document.getElementById('tutorialOverlay');
+    overlay.classList.remove('hidden');
+
+    const maskedElements = document.getElementsByClassName('maskedElement');
+    for (let i = 0; i < maskedElements.length; i++) {
+        maskedElements[i].classList.remove('maskedElement');
+    }
+
+    const content = document.getElementById('overlayContent');
+    const mask = document.getElementById('overlayMask');
+
+    element.scrollIntoView();
+    element.classList.add('maskedElement');
+
+    const bounds = element.getBoundingClientRect();
+    const x = bounds.left;
+    const y = bounds.bottom + 10;
+
+    content.style.left = x + 'px';
+    content.style.top = y + 'px';
+    content.innerHTML = text;
+
+    const maskPadding = 5;
+
+    const maskX = bounds.left;
+    const maskY = bounds.top;
+
+    const maskWidth = (bounds.right - bounds.left) + (2 * maskPadding);
+    const maskHeight = (bounds.bottom - bounds.top) + (2 * maskPadding);
+
+    mask.style.left = maskX + 'px';
+    mask.style.top = maskY + 'px';
+    mask.style.width = maskWidth + 'px';
+    mask.style.height = maskHeight + 'px';
+
+    overlay.addEventListener('click', next, {once: true});
+}
+
+function hideOverlay() {
+    document.getElementById('tutorialOverlay').classList.add('hidden');
+
+    const mask = document.getElementById('overlayMask');
+    mask.style.top = "";
+    mask.style.left = "";
+    mask.style.width = "";
+    mask.style.height = "";
+
+    const content = document.getElementById('overlayContent');
+    content.style.top = "";
+    content.style.left = "";
+
+    const maskedElements = document.getElementsByClassName('maskedElement');
+    for (let i = 0; i < maskedElements.length; i++) {
+        maskedElements[i].classList.remove('maskedElement');
+    }
+}
