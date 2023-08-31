4b3654230f90e267e84a70d336776d0e9043ebf5
colebot17 <colebot17@gmail.com>
2023-08-31 12:37:06 -0400

    Remove commented-out code

--- scripts/script.js
+++ scripts/script.js
@@ -284,15 +284,10 @@ function loadGame(id = prompt("Enter the id of the game you want to load:"), ani
 			}, 10);
 
 			animationCleanup = () => {
-				// const oldHeight = parseInt(dupEl.style.height.slice(0, -2));
-				// const oldTop = parseInt(dupEl.style.top.slice(0, -2));
-				// dupEl.style.opacity = "0%";
-				// dupEl.style.height = "0";
 				clearInterval(flash);
 
 				dupEl.style.opacity = "0%";
 				dupEl.style.scale = "5";
-				// dupEl.style.top = oldTop + (oldHeight / 2) + 'px';
 				dupEl.style.background = "var(--background-3)";
 				dupEl.style.pointerEvents = "none";
 				dupEl.innerHTML = "";
