1dd4b2c950bca4b46ae798d5973704eea9dcd79c
colebot17 <colebot17@gmail.com>
2023-09-01 15:10:42 -0400

    Add a space

--- scripts/script.js
+++ scripts/script.js
@@ -325,6 +325,8 @@ function loadGame(id = prompt("Enter the id of the game you want to load:"), ani
 
 		el.style.lineHeight = '100vh';
 
+		el.innerHTML = " ";
+
 		let timeout = setTimeout(() => {
 			el.innerHTML = "Loading Game";
 		}, 500);
