f37a0578b51775464046620b2e595e7e87aa0920
colebot17 <colebot17@gmail.com>
2023-09-01 12:44:48 -0400

    Sanity check

--- scripts/autoLoader.js
+++ scripts/autoLoader.js
@@ -1,6 +1,7 @@
 function checkParams() {
     const params = new URLSearchParams(window.location.search);
     if (params.has('game')) {
-        loadGame(parseInt(params.get('game')));
+       //loadGame(parseInt(params.get('game')));
+       console.log(params.get('game'));
     }
 }
