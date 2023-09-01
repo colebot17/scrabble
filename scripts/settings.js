a2cca8d6edae258a9f0dfbbcd651ff24dbfb3010
colebot17 <colebot17@gmail.com>
2023-08-31 21:39:00 -0400

    Create a system for linked settings

---
+++ scripts/settings.js
@@ -0,0 +1,37 @@
+class Setting {
+    constructor(id, linkedEl, linkedElProperty, initSubscribers = []) {
+        this.id = id;
+        this.value = linkedEl[linkedElProperty];
+        this.linkedEl = linkedEl;
+        this.linkedElProperty = linkedElProperty;
+        this.subscribers = initSubscribers;
+
+        // set up the linked element
+        this.linkedEl.addEventListener('input', this.#linker);
+    }
+
+    #linker() {
+        this.#setValue(this.linkedEl[this.linkedElProperty]);
+    }
+
+    #setValue(value) {
+        this.value = value;
+        for (let i = 0; i < this.subscribers.length; i++) {
+            this.subscribers[i](value);
+        }
+    }
+
+    subscribe(callback) {
+        this.subscribers.push(callback);
+    }
+
+    unsubscribe(callback) {
+        const index = this.subscribers.indexOf(callback);
+        if (typeof index !== 'number') return null;
+        return this.subscribers.splice(index, 1);
+    }
+}
+
+var settings = {
+    "hideChatBox": new Setting('hideChatBox', document.getElementById('hideChatBoxToggle'), 'checked')
+};
