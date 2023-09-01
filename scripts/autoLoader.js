4cbe7b5fba1d3e46a8c4647ef059af8d16a52061
colebot17 <colebot17@gmail.com>
2023-09-01 12:44:10 -0400

    Try parsing int

--- scripts/autoLoader.js
+++ scripts/autoLoader.js
@@ -1,6 +1,6 @@
 function checkParams() {
     const params = new URLSearchParams(window.location.search);
     if (params.has('game')) {
-        loadGame(params.get('game'));
+        loadGame(parseInt(params.get('game')));
     }
 }
