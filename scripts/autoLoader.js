1f7db0a7b9b4c0a128d789fb236da11d69c0b1c0
colebot17 <colebot17@gmail.com>
2023-09-01 12:48:48 -0400

    Actually load the game

--- scripts/autoLoader.js
+++ scripts/autoLoader.js
@@ -1,7 +1,6 @@
 function checkParams() {
     const params = new URLSearchParams(window.location.search);
     if (params.has('game')) {
-       //loadGame(parseInt(params.get('game')));
-       console.log(params.get('game'));
+       loadGame(parseInt(params.get('game')));
     }
 }
