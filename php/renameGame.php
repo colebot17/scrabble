bbd7eaeac21f8f06425b936d5dde0835a8882d81
colebot17 <colebot17@gmail.com>
2023-09-24 18:09:12 -0400

    Add ./

--- php/renameGame.php
+++ php/renameGame.php
@@ -59,11 +59,11 @@ $updateData = Array(
 	"newName" => $name
 );
 
-require "./addUpdate.php";
+require "../php/addUpdate.php";
 addUpdate($conn, $gameId, "chatMessageSend", $updateData);
 
 // add system message to chat
-require "./chat/addSystemChatMessage.php";
+require "../php/chat/addSystemChatMessage.php";
 $data = Array(
 	"playerId" => $user,
 	"newName" => $name
