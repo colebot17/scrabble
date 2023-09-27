be9110facda0ed032acb8eae096b4a5423a71028
colebot17 <colebot17@gmail.com>
2023-09-27 12:45:18 -0400

    Make it lighter

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,7 +1,7 @@
 var canvas = {};
 
 const BOARD_BACKGROUND_COLOR = "#f2f5ff";
-const BOARD_COLOR_KEY = ["#00000012", "#6dd0f7", "#1b4afc", "#faaab5", "#ff2c2b", "#faaab5"];
+const BOARD_COLOR_KEY = ["#00000006", "#6dd0f7", "#1b4afc", "#faaab5", "#ff2c2b", "#faaab5"];
 const BOARD_MULTIPLIER_STRINGS = ["", "L2", "L3", "W2", "W3", ""];
 const SQUARE_NUM = 15;
 const SQUARE_GAP = -0.5;
