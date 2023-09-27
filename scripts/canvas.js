6ecf39c545f3b36fd667d02b50d86a2b0231f0e3
colebot17 <colebot17@gmail.com>
2023-09-27 00:17:09 -0400

    Use a more efficient drawing algorithm

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,10 @@
 var canvas = {};
 
-const boardColorKey = ["#f2f5ff", "#6dd0f7", "#1b4afc", "#faaab5", "#ff2c2b", "#faaab5"];
-const boardMultiplierStrings = ["", "L2", "L3", "W2", "W3", ""];
-const squareNum = 15;
-const squareGap = 1;
+const BOARD_BACKGROUND_COLOR = "#f2f5ff";
+const BOARD_COLOR_KEY = ["transparent", "#6dd0f7", "#1b4afc", "#faaab5", "#ff2c2b", "#faaab5"];
+const BOARD_MULTIPLIER_STRINGS = ["", "L2", "L3", "W2", "W3", ""];
+const SQUARE_NUM = 15;
+const SQUARE_GAP = -1;
 var squareWidth;
 
 function canvasInit() {
@@ -135,16 +136,22 @@ function clearCanvas() {
 }
 
 function drawBoard() {
-	// get some sizes
-	squareWidth = (canvas.c.width - (squareGap * (squareNum - 1))) / squareNum;
-	let fontSize = squareWidth * 0.5;
-	for (var y = 0; y < squareNum; y++) { // for each tile
-		for (var x = 0; x < squareNum; x++) {
-			canvas.ctx.fillStyle = boardColorKey[boardModifiers[y][x]];
-			let xPos = (x * squareWidth) + (x * squareGap);
-			let yPos = (y * squareWidth) + (y * squareGap);
+	// calculate some values
+	squareWidth = (canvas.c.width - (SQUARE_GAP * (SQUARE_NUM - 1))) / SQUARE_NUM;
+	const fontSize = squareWidth * 0.5;
+	const cornerRadius = 5 * (squareWidth * 0.03);
 
-			const cornerRadius = 5 * (squareWidth * 0.03);
+	// draw the background
+
+
+	for (var y = 0; y < SQUARE_NUM; y++) { // for each tile
+		for (var x = 0; x < SQUARE_NUM; x++) {
+			const squareColor = BOARD_COLOR_KEY[boardModifiers[y][x]];
+			if (squareColor === "transparent") continue; // skip regular/transparent tiles since they are the same as the background
+
+			canvas.ctx.fillStyle = squareColor;
+			let xPos = (x * squareWidth) + (x * SQUARE_GAP);
+			let yPos = (y * squareWidth) + (y * SQUARE_GAP);
 
 			// round the corners of the board
 			let radii = {
@@ -158,11 +165,11 @@ function drawBoard() {
 			roundRect(canvas.ctx, xPos, yPos, squareWidth, squareWidth, radii);
 			//squareWidth >= 35 &&
 			// if size permits, show the board multiplier strings
-			if ( boardMultiplierStrings[boardModifiers[y][x]]) {
+			if (BOARD_MULTIPLIER_STRINGS[boardModifiers[y][x]]) {
 				canvas.ctx.font = fontSize + "px Rubik";
 				canvas.ctx.fillStyle = "#f2f5ff";
 				canvas.ctx.textAlign = "center";
-				canvas.ctx.fillText(boardMultiplierStrings[boardModifiers[y][x]], (x * squareWidth) + (x * squareGap) + (squareWidth / 2), (y * squareWidth) + (y * squareGap) + (squareWidth / 2) + (fontSize / 2.5));
+				canvas.ctx.fillText(BOARD_MULTIPLIER_STRINGS[boardModifiers[y][x]], (x * squareWidth) + (x * SQUARE_GAP) + (squareWidth / 2), (y * squareWidth) + (y * SQUARE_GAP) + (squareWidth / 2) + (fontSize / 2.5));
 			}
 		}
 	}
@@ -418,8 +425,8 @@ function updateTile(tile) {
 	var tileWidth = squareWidth * tileSize;
 	var fontSize = tileWidth * 0.83;
 
-	var pixelX = (tile.pixelX + (tile.mouseOffset?.x || -(squareWidth / 2)) || (tile.x * squareWidth) + (tile.x * squareGap)) + ((squareWidth - tileWidth) / 2);
-	var pixelY = (tile.pixelY + (tile.mouseOffset?.y || -(squareWidth / 2)) || (tile.y * squareWidth) + (tile.y * squareGap)) + ((squareWidth - tileWidth) / 2);
+	var pixelX = (tile.pixelX + (tile.mouseOffset?.x || -(squareWidth / 2)) || (tile.x * squareWidth) + (tile.x * SQUARE_GAP)) + ((squareWidth - tileWidth) / 2);
+	var pixelY = (tile.pixelY + (tile.mouseOffset?.y || -(squareWidth / 2)) || (tile.y * squareWidth) + (tile.y * SQUARE_GAP)) + ((squareWidth - tileWidth) / 2);
 
 	// draw the tile
 	canvas.ctx.fillStyle = (tile.locked ? "#a47449" : "#a47449cc"); // tile brown
@@ -485,10 +492,10 @@ function drawRegions(regions) {
 		canvas.ctx.font = fontSize + "px Rubik";
 
 		// draw a rectangle around the affected letters
-		let x1 = regions[i].start[0] * (squareWidth + squareGap);
-		let y1 = regions[i].start[1] * (squareWidth + squareGap);
-		let x2 = (regions[i].end[0] * (squareWidth + squareGap)) + squareWidth;
-		let y2 = (regions[i].end[1] * (squareWidth + squareGap)) + squareWidth;
+		let x1 = regions[i].start[0] * (squareWidth + SQUARE_GAP);
+		let y1 = regions[i].start[1] * (squareWidth + SQUARE_GAP);
+		let x2 = (regions[i].end[0] * (squareWidth + SQUARE_GAP)) + squareWidth;
+		let y2 = (regions[i].end[1] * (squareWidth + SQUARE_GAP)) + squareWidth;
 
 		// calculate position for the bubble
 		let circX = x2;
