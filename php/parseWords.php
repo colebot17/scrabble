c349076c1e857b3caa8eb2909bea71835e24e684
colebot17 <colebot17@gmail.com>
2023-10-01 14:15:04 -0500

    Check letter validity properly

--- php/parseWords.php
+++ php/parseWords.php
@@ -42,9 +42,9 @@ function parseWords($gameId, $tiles, $user) {
         }
     
         // make sure player owns all letters being placed
-        // if ($players[$currentPlayerIndex]["letterBank"][$tiles[$tiles[$i]["bankIndex"]]] !== $tiles[$i]['letter']) {
-        //     return '{"errorLevel":2,"message":"You must own all letters being used."}';
-        // }
+        if ($players[$currentPlayerIndex]["letterBank"][$tiles[$i]["bankIndex"]] !== $tiles[$i]['letter']) {
+            return '{"errorLevel":2,"message":"You must own all letters being used."}';
+        }
     
         // generate a tile with only the information we need
         $tile = Array(
