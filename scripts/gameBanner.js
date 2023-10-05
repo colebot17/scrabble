1036214d3425bcb49e495ceaad5f5c9172453f88
colebot17 <colebot17@gmail.com>
2023-10-05 16:26:20 -0400

    Indroduce modules with a moduleScript file

---
+++ scripts/gameBanner.js
@@ -0,0 +1,52 @@
+export async function gameBanner(content, color, textColor = "", temp = false) {
+	const wrapper = document.getElementById('gameBannerWrapper');
+	const banner = document.getElementById('gameBanner');
+	if (content) {
+		banner.innerHTML = content;
+		banner.style.backgroundColor = color;
+		banner.style.color = textColor;
+
+		wrapper.classList.remove('hidden');
+
+		if (temp) {
+			const bottom = wrapper.getBoundingClientRect().bottom;
+			wrapper.style.position = "absolute";
+			wrapper.style.top = "-" + bottom + "px";
+			wrapper.style.transition = "top 0.2s cubic-bezier(0.33333, 0, 0.66667, 0.33333)";
+
+			banner.classList.add('tempBanner');
+
+			setCanvasSize();
+
+			await sleep(10);
+
+			wrapper.style.top = "10px";
+
+			await sleep(1500);
+
+			wrapper.style.transition = "top 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1)";
+			wrapper.style.top = "-" + bottom + "px";
+
+			await sleep(200);
+
+			wrapper.style.position = "";
+			wrapper.style.transition = "";
+			wrapper.style.top = "";
+
+			banner.classList.remove('tempBanner');
+
+			await gameBanner();
+
+			setCanvasSize();
+		} else {
+			setCanvasSize();
+		}
+	} else {
+		banner.innerHTML = '';
+		banner.style.backgroundColor = '';
+		banner.style.color = '';
+		wrapper.classList.add('hidden');
+
+		setCanvasSize();
+	}
+}
---
+++ scripts/moduleScript.js
@@ -0,0 +1,1 @@
+import { gameBanner } from "/gameBanner.js";
