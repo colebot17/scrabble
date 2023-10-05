31b41326c10d4bd77bf259eb358258c6b5752a5b
colebot17 <colebot17@gmail.com>
2023-10-05 16:40:42 -0400

    Add a comment

--- scripts/moduleScript.js
+++ scripts/moduleScript.js
@@ -1,3 +1,4 @@
-import { gameBanner as gameBanner_ } from "./gameBanner.js";
+// This is an intermediate step to allow things to be progressively moved over to using modules
 
+import { gameBanner as gameBanner_ } from "./gameBanner.js";
 window.gameBanner = gameBanner_;
