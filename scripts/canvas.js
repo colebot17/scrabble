284f8b4b85e9f9f199ef3506b860c54643880f39
colebot17 <colebot17@gmail.com>
2023-10-05 21:07:28 -0400

    Change board background color based on theme

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -144,7 +144,8 @@ function drawBoard() {
 	const cornerRadius = 5 * (squareWidth * 0.03);
 
 	// draw the background
-	canvas.ctx.fillStyle = BOARD_BACKGROUND_COLOR;
+	const boardBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-0');
+	canvas.ctx.fillStyle = boardBackgroundColor === '#f2f5ff' ? BOARD_BACKGROUND_COLOR : boardBackgroundColor;
 	roundRect(canvas.ctx, 0, 0, canvas.c.width, canvas.c.width, cornerRadius);
 
 	for (var y = 0; y < SQUARE_NUM; y++) { // for each tile
