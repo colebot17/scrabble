b2528e7a8e4e303c0fb7b8afcd0a25948a4c4994
colebot17 <colebot17@gmail.com>
2023-10-06 10:21:35 -0400

    Migrate all games in one go

--- php/temp/migrateBoard.php
+++ php/temp/migrateBoard.php
@@ -12,28 +12,29 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$game = $_GET['game'];
-
-$sql = "SELECT board FROM games WHERE id='$game'";
+$sql = "SELECT board, id FROM games";
 $query = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($query);
 
-$board = json_decode($row['board'], true);
+while ($row = mysqli_fetch_assoc($query)) {
 
-for ($y = 0; $y < count($board); $y++) {
-    for ($x = 0; $x < count($board); $x++) {
-        if ($board[$y][$x]) {
-            $board[$y][$x]["x"] = $x;
-            $board[$y][$x]["y"] = $y;
+    $board = json_decode($row['board'], true);
+
+    for ($y = 0; $y < count($board); $y++) {
+        for ($x = 0; $x < count($board); $x++) {
+            if ($board[$y][$x]) {
+                $board[$y][$x]["x"] = $x;
+                $board[$y][$x]["y"] = $y;
+            }
         }
     }
+
+    $boardJson = json_encode($board);
+
+    $sql2 = "UPDATE games SET board='$boardJson' WHERE id='$game'";
+    $query2 = mysqli_query($conn, $sql2);
+
+    echo "Updated " . $row['id'] . "!<br>";
+
 }
-
-$boardJson = json_encode($board);
-
-$sql = "UPDATE games SET board='$boardJson' WHERE id='$game'";
-$query = mysqli_query($conn, $sql);
-
-echo "Updated!";
 
 ?>
