3f8eaaa392c62b81e79bdbaa68bfdab2ca95b13f
colebot17 <colebot17@gmail.com>
2023-10-06 20:22:49 -0400

    Average square color for alternate themes

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -151,7 +151,7 @@ function drawBoard() {
 
 	for (var y = 0; y < SQUARE_NUM; y++) { // for each tile
 		for (var x = 0; x < SQUARE_NUM; x++) {
-			const squareColor = BOARD_COLOR_KEY[boardModifiers[y][x]];
+			const squareColor = hasAltTheme ? averageColor(BOARD_COLOR_KEY[boardModifiers[y][x]], themedBoardBackgroundColor) : BOARD_COLOR_KEY[boardModifiers[y][x]];
 			const squareType = BOARD_SQUARE_TYPES[boardModifiers[y][x]];
 			const squareContents = SQUARE_CONTENTS[boardModifiers[y][x]];
 			if (squareColor === "transparent") continue; // skip regular/transparent tiles since they are the same as the background
--- scripts/moduleScript.js
+++ scripts/moduleScript.js
@@ -2,3 +2,6 @@
 
 import { gameBanner as gameBanner_ } from "./gameBanner.js";
 window.gameBanner = gameBanner_;
+
+import { averageColor as averageColor_ } from "//www.colebot.com/colors.js";
+window.averageColor = averageColor_;
