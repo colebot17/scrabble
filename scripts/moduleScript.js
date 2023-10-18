7949ebb6529b08fcfbdc3d1b8b24b57b479fe220
colebot17 <colebot17@gmail.com>
2023-10-18 13:26:24 -0400

    Multiply board square colors instead of averaging

--- scripts/moduleScript.js
+++ scripts/moduleScript.js
@@ -3,5 +3,6 @@
 import { gameBanner as gameBanner_ } from "./gameBanner.js";
 window.gameBanner = gameBanner_;
 
-import { averageColor as averageColor_ } from "//www.colebot.com/colors.js";
+import { averageColor as averageColor_, multiplyColor as multiplyColor_ } from "//www.colebot.com/colors.js";
 window.averageColor = averageColor_;
+window.multiplyColor = multiplyColor_;
