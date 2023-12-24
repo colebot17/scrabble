6a724a91aa3ada38c012260ee2a220d9b2096baa
colebot17 <colebot17@gmail.com>
2023-12-24 15:45:46 -0500

    Disable move button when board is empty of new tiles

--- scripts/board.js
+++ scripts/board.js
@@ -24,6 +24,30 @@ function clearBoard() {
 }
 
 function boardUpdate() {
-    // this function is called whenever the tiles present on the board are changed in any way
-    console.log("boardUpdate");
+    // this function is called immediately after the tiles present on the board are changed in any way
+    
+    setMoveButtonEnablement();
+}
+
+function setMoveButtonEnablement() {
+    const userTurn = !game.inactive && game.players[parseInt(game.turn) % game.players.length].id == account.id;
+    if (!userTurn) return;
+    // the move button will be disabled no matter what when it is not the current user's turn
+
+    // make sure there are actually some unlocked tiles on the board
+    const boardHasUnlockedTiles = false;
+    for (let y = 0; y < game.board.length; y++) {
+        for (let x = 0; x < game.board[y].length; x++) {
+            if (!game.board[y][x].locked) {
+                boardHasUnlockedTiles = true;
+                break;
+            }
+        }
+    }
+
+    const moveButton = document.getElementById('makeMoveButton');
+    moveButton.disabled = !boardHasUnlockedTiles;
+    if (boardHasUnlockedTiles) {
+        moveButton.title = "You must place tiles on the board to make a move.";
+    }
 }
