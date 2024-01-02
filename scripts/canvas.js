b906019d6c626afef8a933045ad5a850471cae0d
colebot17 <colebot17@gmail.com>
2024-01-02 12:05:28 -0500

    Fix typo

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -239,8 +239,8 @@ function drawLetterBank() {
 			canvas.ctx.fillText(game.lettersLeft, numberStartX, lbcY);
 
 			canvas.ctx.strokeStyle = "red";
-			canvas.ctx.strokeRect(iconStartX, lbcY, iconStartX + iconWidth, lbcY - lbcSise);
-			canvas.ctx.strokeRect(numberStartX, lbcY, numberStartX + iconWidth, lbcY - lbcSise);
+			canvas.ctx.strokeRect(iconStartX, lbcY, iconStartX + iconWidth, lbcY - lbcSize);
+			canvas.ctx.strokeRect(numberStartX, lbcY, numberStartX + iconWidth, lbcY - lbcSize);
 
 
 
