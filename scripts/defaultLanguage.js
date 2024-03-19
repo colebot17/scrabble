19a94f84c2c569a814baa4d72b2294c7c14ae0e4
colebot17 <colebot17@gmail.com>
2024-03-19 13:39:04 -0400

    Show error if it occurs

--- scripts/defaultLanguage.js
+++ scripts/defaultLanguage.js
@@ -4,11 +4,14 @@ function initDefaultLanguageSelectors() {
     for (let el of selectors) {
         el.addEventListener('change', async () => {
             const selectedLang = document.querySelector('.defaultLanguageSelector:checked').value;
-            await request('setDefaultLang.php', {
+            const res = await request('setDefaultLang.php', {
                 user: account.id,
                 pwd: account.pwd,
                 newDefaultLang: selectedLang
             });
+            if (res.errorLevel) {
+                textModal('Error', res.message);
+            }
         });
     }
 }
