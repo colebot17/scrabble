d56656e5886b345526fb139f96912affcf5f0015
colebot17 <colebot17@gmail.com>
2024-03-19 13:10:25 -0400

    Automatically select current default language

--- admin/changeDefaultLang.php
+++ admin/changeDefaultLang.php
@@ -11,33 +11,23 @@
 <body>
     <h3>This is the admin panel for scrabble.colebot.com<br><a href="index.php">Admin Home</a></h3>
     <h1>Change Default Language</h1>
-    <form method="POST">
-        <?php echo '<input type="text" name="username"' . (array_key_exists('user', $_GET) ? ' value="' . $_GET['user'] . '"' : '') . ' placeholder="Username">';?>
-        <select name="newDefaultLang">
-            <option value="english">English</option>
-            <option value="spanish">Spanish</option>
-        </select>
-        <br>
-        <button>Change Default Language</button>
-    </form>
+    <?php 
+    
+    // define connection
+    $servername = "173.201.180.187";
+    $username = "Colebot";
+    $password = "96819822";
+    $dbname = "scrabble";
 
-    <?php
+    // create and check connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
     if (array_key_exists('username', $_POST) && array_key_exists('newDefaultLang', $_POST) && $_POST['username'] !== "" && $_POST['newDefaultLang'] !== "") {
         $un = $_POST['username'];
         $newDefaultLang = $_POST['newDefaultLang'];
-        
-        // define connection
-        $servername = "173.201.180.187";
-        $username = "Colebot";
-        $password = "96819822";
-        $dbname = "scrabble";
-
-        // create and check connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
 
         $sql = "UPDATE accounts SET defaultLang='$newDefaultLang' WHERE name='$un'";
         $query = mysqli_query($conn, $sql);
@@ -47,9 +37,35 @@
             echo '<br><br><span>Default Language Changed to ' . $newDefaultLang . '</span>';
         }
 
-        echo '<br><a href="playerLookup.php?playerName=' . $newUsername . '">Info</a>';
-    }
+        echo '<br><a href="playerLookup.php?playerName=' . $un . '">Info</a>';
 
+        exit();
+    }
+    
+    echo '<form method="POST">';
+    
+        echo '<input type="text" name="username"' . (array_key_exists('user', $_GET) ? ' value="' . $_GET['user'] . '"' : '') . ' placeholder="Username">';
+
+        if ($_GET['user']) {
+            $gottenName = $_GET['user'];
+            $sql = "SELECT defaultLang FROM accounts WHERE name='$gottenName'";
+            $query = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($query);
+            $currentDefaultLang = $row['defaultLang'];
+        }
+
+        $langOptions = Array("english", "spanish");
+
+        echo '<select name="newDefaultLang">';
+        for ($i = 0; $i < count($langOptions); $i++) {
+            echo '<option value="' . $langOptions[$i] . '"' . ($langOptions[$i] === $currentDefaultLang ? ' selected' : '') . '>' . $langOptions[$i] . '</option>';
+        }
+
+        echo '<br>';
+        echo '<button>Change Default Language</button>';
+        
+    echo '</form>';
+    
     ?>
 </body>
 </html>
