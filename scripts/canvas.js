9087c63ad828b3990aaaa1c2b3f052f6529aef14
colebot17 <colebot17@gmail.com>
2024-03-30 08:57:08 +0100

    Fix typo

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -301,8 +301,8 @@ function drawLetterBank() {
 				canvas.ctx.fillStyle = textColor;
 				canvas.ctx.font = lbcSize + "px Rubik";
 				canvas.ctx.textAlign = "left";
-				canvas.ctx.textBaseline = "middle";
-				canvas.ctx.fillText("New", textX, y);
+				canvas.ctx.textBaseline = "alphabetic";
+				canvas.ctx.fillText("New", textX, y + (lbcSize / 2));
 
 				canvas.ctx.restore();
 			}
