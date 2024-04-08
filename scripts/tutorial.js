728c3c364e2d7ae1708295edde583b72e0d56c6c
colebot17 <colebot17@gmail.com>
2024-04-08 14:22:36 -0400

    Change first selector

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -27,7 +27,7 @@ const firstGameTutorial = [
 ];
 
 const settingsTutorial = [
-    new TutorialStep('#settingsForm h3', "Welcome to Settings! Click anywhere to learn more."),
+    new TutorialStep('#signInCell', "Welcome to Settings! Click anywhere to learn more."),
     new TutorialStep('#displayModeSettingSection', "If you have a small screen, try changing your view to fit more onscreen."),
     new TutorialStep('#chatSettingSection', "For distraction-free gameplay, you can hide the chat box on wide screens."),
     new TutorialStep('#languageSettingSection', "You can choose your default language here. You can still change a game's language individually when you create it.")
