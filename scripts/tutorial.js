feb9adde335cbcb7929efb4ec637c18cf0b6b54e
colebot17 <colebot17@gmail.com>
2024-04-08 14:24:57 -0400

    Add a step

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -28,6 +28,7 @@ const firstGameTutorial = [
 
 const settingsTutorial = [
     new TutorialStep('#signInCell', "Welcome to Settings! Click anywhere to learn more."),
+    new TutorialStep('#accountSettingsSection', "Here are some useful actions for your account."),
     new TutorialStep('#displayModeSettingSection', "If you have a small screen, try changing your view to fit more onscreen."),
     new TutorialStep('#chatSettingSection', "For distraction-free gameplay, you can hide the chat box on wide screens."),
     new TutorialStep('#languageSettingSection', "You can choose your default language here. You can still change a game's language individually when you create it.")
