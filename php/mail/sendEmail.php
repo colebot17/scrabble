08938a5961574fa3eb7c2121a6a290c7a89eda49
colebot17 <colebot17@gmail.com>
2024-04-17 14:11:04 -0400

    Use correct require path

--- php/mail/sendEmail.php
+++ php/mail/sendEmail.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
