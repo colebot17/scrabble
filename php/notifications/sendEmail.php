a256acee4908f2e08893e9ed005847bdcfab2ea1
colebot17 <colebot17@gmail.com>
2024-04-17 20:06:30 -0400

    Test sending notification to stored email

---
+++ php/notifications/sendEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+function sendEmail($to, $subject, $body, $html = true) {
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'mail.colebot.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'colebot@colebot.com';                     //SMTP username
+        $mail->Password   = 'Colebot@96819822';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom('colebot@colebot.com', 'Colebot Himself');
+        $mail->addAddress($to); //Add the recipient
+        //$mail->addReplyTo('colebot@colebot.com', 'Colebot');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    
+        //Content
+        $mail->isHTML($html);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AltBody = $body;
+    
+        $mail->send();
+        return Array("errorLevel" => 0, "message" => "Email Sent");
+    } catch (Exception $e) {
+        return Array("errorLevel" => 2, "message" => "There was an error sending the email: " . $mail->ErrorInfo);
+    }
+}
---
+++ php/notifications/sendTurnNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+require "sendEmail.php";
+
+function sendTurnNotification($conn, $user, $game) {
+
+    $sql = "SELECT notificationMethods FROM accounts WHERE id='$user'";
+    $query = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($query);
+
+    $methods = json_decode($row['notificationMethods'], true);
+
+    for ($i = 0; $i < count($methods); $i++) {
+        if ($methods[$i]["type"] === "email") {
+            sendEmail($methods[$i]["address"], "Test Email", "This will eventually become the turn notification email.");
+        }
+    }
+
+    //sendEmail($to, $subject, $body);
+}
+
+// define connection
+$servername = "173.201.180.187";
+$username = "Colebot";
+$password = "96819822";
+$dbname = "scrabble";
+
+// create and check connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+sendTurnNotification($conn, 1, 476);
