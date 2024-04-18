812861fe79ad1dc060aa3c0072fbe0371b2a9fbd
colebot17 <colebot17@gmail.com>
2024-04-17 20:16:11 -0400

    Change from address

--- php/notifications/sendEmail.php
+++ php/notifications/sendEmail.php
@@ -23,7 +23,7 @@ function sendEmail($to, $subject, $body, $html = true) {
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
         //Recipients
-        $mail->setFrom('colebot@colebot.com', 'Colebot Himself');
+        $mail->setFrom('scrabble@colebot.com', 'Scrabble.colebot.com');
         $mail->addAddress($to); //Add the recipient
         //$mail->addReplyTo('colebot@colebot.com', 'Colebot');
         //$mail->addCC('cc@example.com');
