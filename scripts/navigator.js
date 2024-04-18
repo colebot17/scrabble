98dd4023838372b9161d0b8c58b65b93d04df3f7
colebot17 <colebot17@gmail.com>
2024-04-18 09:19:55 -0400

    Make sure the game id is a number

--- scripts/navigator.js
+++ scripts/navigator.js
@@ -3,7 +3,13 @@ function checkParams() {
     if (params.has('game')) {
         const gameId = parseInt(params.get('game'));
 
-        // find the game in the user's game list
+        // make sure the game id is a number
+        if (isNaN(gameId)) {
+            textModal("Invalid game id", "The url-specified game is not a valid game id. Check your link and try again.");
+            return;
+        }
+
+        // make sure the game is in the user's game list
         if (!account?.games?.find(a => a.id === gameId)) {
             textModal("Game not found", "You are trying to load a game that you don't have access to. Sign in to the correct account to access game <b>#" + gameId + "</b>");
             return;
