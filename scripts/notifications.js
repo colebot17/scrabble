dbce61738d0ffb8d0ae17eddc6f4c318f483f7d9
colebot17 <colebot17@gmail.com>
2024-04-18 09:54:56 -0400

    Add a notification manager modal

---
+++ scripts/notifications.js
@@ -0,0 +1,18 @@
+function manageNotifications() {
+    $('notificationManagerModal').modalOpen();
+}
+
+async function addEmail(email) {
+    // validate email
+
+    const res = await request('notifications/addEmail.php', {
+        user: account.id,
+        pwd: account.pwd,
+        email: email
+    });
+
+    if (res.errorLevel) {
+        textModal("Error", res.message);
+        return;
+    }
+}
