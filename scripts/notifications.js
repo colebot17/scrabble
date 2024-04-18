d617da5c091c4ef82e8935bad35624ff133ef172
colebot17 <colebot17@gmail.com>
2024-04-18 10:20:12 -0400

    Show notification methods

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,5 +1,6 @@
 function manageNotifications() {
     $('#notificationManagerModal').modalOpen();
+    displayNotificationMethods();
 }
 
 async function addEmail(email) {
@@ -15,4 +16,30 @@ async function addEmail(email) {
         textModal("Error", res.message);
         return;
     }
+}
+
+function displayNotificationMethods() {
+    const container = document.getElementById('notificationMethodsList');
+    container.innerHTML = "";
+
+    for (let i = 0; i < account.notificationMethods.length; i++) {
+        const method = account.notificationMethods[i];
+        if (method.type !== email) continue;
+
+        const content = /* html */ `
+            <div class="friendListItem emailNotificationMethod">
+                <div class="flex flexStart">${method.address}</div>
+                <button class="iconButton" onclick="removeNotificationMethod(${i})"><span class="material-symbols-rounded">remove</span></button>
+            </div>
+        `;
+
+        container.innerHTML += content;
+    }
+}
+
+function removeNotificationMethod(index) {
+    // remove the notification method
+    account.notificationMethods.splice(index, 1);
+
+    displayNotificationMethods();
 }
