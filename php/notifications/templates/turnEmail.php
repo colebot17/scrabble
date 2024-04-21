1325e867b2a92f08107f63bc4f883def333aa1ff
colebot17 <colebot17@gmail.com>
2024-04-20 22:54:45 -0400

    Add a way to unsubscribe

--- php/notifications/templates/turnEmail.php
+++ php/notifications/templates/turnEmail.php
@@ -16,7 +16,7 @@ function turnEmail($prevPlayerName, $gameName, $gameId, $playerNames) {
                 background-color: var(--background-0);
             }
         </style>
-        <h1><b>' . $prevPlayerName . '</b> made their move on Scrabble!</h1>
+        <h1><b>' . $prevPlayerName . '</b> just made their move on Scrabble!</h1>
         <div class="gameCard" style="min-width:150px;background-color:#6BBAEC;border-radius:10px;padding:10px;text-align:center;display:inline-block;border:3px solid #FF57BB;">
             ' . $gameCardTitleLine . '
             <br>
@@ -34,7 +34,6 @@ function turnEmail($prevPlayerName, $gameName, $gameId, $playerNames) {
         </div>
         <h2>It\'s your turn now, so go make your move!</h2>
         <p>Alternatively, copy and paste this link into your browser:<br>' . $playLink . '</p>
-        <p>You are receiving this email because you signed up for notifications on <a href="https://scrabble.colebot.com">scrabble.colebot.com</a>.</p>
     ';
 
     return $code;
