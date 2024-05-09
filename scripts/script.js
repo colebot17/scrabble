9f5df2a29cab31f46459bdd742b291ab249be914
colebot17 <colebot17@gmail.com>
2024-05-08 21:05:52 -0400

    Change timing function

--- scripts/script.js
+++ scripts/script.js
@@ -900,7 +900,7 @@ function flyingSaucer(from, value, destination, duration = 1000) {
 
 	setTimeout(() => {
 		const d = (duration / 1000) + 's';
-		const e = "ease-in"
+		const e = "cubic-bezier(.48,.08,.83,.67)";
 		destBounds = destination.getBoundingClientRect();
 
 		saucer.style.transition = `top ${d} ${e}, left ${d} ${e}`;
