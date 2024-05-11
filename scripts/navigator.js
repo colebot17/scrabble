996a0f57fdd587f2307d538aac1dbdd029e6cac3
colebot17 <colebot17@gmail.com>
2024-05-11 18:58:50 -0400

    Refactor logic

--- scripts/navigator.js
+++ scripts/navigator.js
@@ -9,40 +9,56 @@ function checkParams() {
             return;
         }
 
-        // make sure the game is in the user's game list
-        if (!account?.games?.find(a => a.id === gameId)) {
-            // if there are multiple accounts saved
-            if (localStorage.savedAccounts) {
-                let accs = JSON.parse(localStorage.savedAccounts);
-                if (accs.length > 0) {
-                    const sGrid = document.getElementById('scrabbleGrid');
-                    sGrid.dataset.signedin = 'loading';
+        openGame(gameId);
+    }
+}
 
-                    let found = false;
-                    request('findGameOwner.php', {
-                        accounts: localStorage.savedAccounts,
-                        gameId: gameId
-                    }).then(res => {
-                        if (res.errorLevel > 0) {
-                            if (res.errorLevel > 1) textModal("Error", res.message);
-                            return;
-                        }
+function openGame(gameId) {
+    // logical flow to find the game
+        
+    const gameFindError = () => textModal("Game not found", "You are trying to load a game that you don't have access to. Sign in to the correct account to access game <b>#" + gameId + "</b>");
 
-                        const ownerAcc = accs[res.data];
-                        signIn(ownerAcc.name, ownerAcc.pwd).then(() => {
-                            loadGame(gameId, 'scrabbleLoader');
-                        });
-                        found = true;
+    // does the game exist in the current account?
+    const inCurrentAccount = account?.games?.find(a => a.id === gameId);
+    if (inCurrentAccount) {
+        // yes: load game
+        loadGame(gameId, 'scrabbleLoader');
+    } else {
+        // no: are there any saved accounts that can be checked?
+        if (localStorage.savedAccounts && JSON.parse(localStorage.savedAccounts).length > 0) {
+            // yes: do any of those accounts own the game? (server request)
+
+            const sGrid = document.getElementById('scrabbleGrid');
+            const oldSGridValue = sGrid.dataset.signedin;
+            sGrid.dataset.signedin = 'loading'; // keep showing the scrabble loader
+            
+            request('findGameOwner.php', {
+                accounts: localStorage.savedAccounts,
+                gameId: gameId
+            }).then(res => {
+                if (res.errorLevel > 0) {
+                    // no: show error message
+                    sGrid.dataset.signedin = oldSGridValue;
+                    gameFindError();
+                } else {
+                    // yes: sign in to that account => load game
+                    const acc = JSON.parse(localStorage.savedAccounts)[res.data];
+                    signIn(acc.name, acc.pwd).then(() => {
+                        loadGame(gameId, 'scrabbleLoader');
                     });
 
-                    if (found) return;
+                    // here would be where we show the message informing the user of the account change
                 }
-            }
-            textModal("Game not found", "You are trying to load a game that you don't have access to. Sign in to the correct account to access game <b>#" + gameId + "</b>");
-            return;
+            }).catch(err => {
+                // network error (make the user think it never happened)
+                sGrid.dataset.signedin = oldSGridValue;
+                console.error(err);
+                gameFindError();
+            });
+        } else {
+            // no: show error message
+            gameFindError();
         }
-
-        loadGame(gameId, 'scrabbleLoader');
     }
 }
 
