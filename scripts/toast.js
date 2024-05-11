0784ec73ffaed6c1e065f1e5cef140190b87d3b7
colebot17 <colebot17@gmail.com>
2024-05-11 19:36:29 -0400

    Add color variation

--- scripts/toast.js
+++ scripts/toast.js
@@ -1,4 +1,4 @@
-export function toast(title, content, duration = 3000) {
+export function toast(title, content, duration = 3000, urgent = false) {
     let toastStack = document.getElementById('toastStack');
     if (!toastStack) {
         toastStack = document.createElement('div');
@@ -8,8 +8,11 @@ export function toast(title, content, duration = 3000) {
 
     const toast = document.createElement('div');
     toast.className = "toast";
+    if (urgent) toast.classList.add('urgent');
     toast.innerHTML = /* html */ `<span class="toastTitle"><b>${title}</b></span><span>${content}</span>`;
     toastStack.appendChild(toast);
+
+    toast.addEventListener('click', () => toast.remove());
 
     if (duration !== 0) {
         setTimeout(() => {
