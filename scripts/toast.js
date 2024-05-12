c71c9beee06491acadd8975830ecf4c377ff8ed1
colebot17 <colebot17@gmail.com>
2024-05-11 20:40:54 -0400

    Add an unused "type" var to the function signature

--- scripts/toast.js
+++ scripts/toast.js
@@ -1,4 +1,4 @@
-export function toast(title, content, duration = 5000, dismissable = true) {
+export function toast(title, content, duration = 5000, _type = "info", dismissable = true) {
     let toastStack = document.getElementById('toastStack');
     if (!toastStack) {
         toastStack = document.createElement('div');
