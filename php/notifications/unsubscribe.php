8cf481bbf1fceeca3a3b81f137fd2d157fd85ca9
colebot17 <colebot17@gmail.com>
2024-05-12 10:02:43 -0400

    Use the correct font on the unsubscribe page

--- php/notifications/unsubscribe.php
+++ php/notifications/unsubscribe.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Unsubscribe</title>
+
+    <!-- import the font Rubik -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+
+    <!-- beautiful css -->
     <link rel="stylesheet" type="text/css" href="https://www.colebot.com/style.css">
 </head>
 <body>
