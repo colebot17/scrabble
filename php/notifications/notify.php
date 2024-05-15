8fe2b62051ceb15a7a3fbaa781917f0b1ef278d7
colebot17 <colebot17@gmail.com>
2024-05-15 12:09:59 -0400

    Make adjustments to greeting

--- php/notifications/notify.php
+++ php/notifications/notify.php
@@ -16,7 +16,7 @@ function notifyByEmail($conn, $user, $subject, $body) {
 
     for ($i = 0; $i < count($methods); $i++) {
         if ($methods[$i]["type"] === "email" && $methods[$i]["enabled"] === true) {
-            $greeting = '<h3>Hey ' . $un . '!</h3>';
+            $greeting = '<h3 style="margin-bottom:-1em">Hey ' . $un . ',</h3>';
             $disclaimer = '<p style="font-size:small">You are receiving this email because you signed up for notifications on <a href="https://scrabble.colebot.com">scrabble.colebot.com</a>. <a href="https://scrabble.colebot.com/php/notifications/unsubscribe.php?email=' . $methods[$i]['address'] . '&user=' . $user . '">Unsubscribe</a></p>';
             sendEmail($methods[$i]["address"], $subject, $greeting . $body . $disclaimer);
         }
