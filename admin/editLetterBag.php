12d02abe714174d60cdc64dd02377d639db4d355
colebot17 <colebot17@gmail.com>
2024-05-24 14:33:19 -0500

    Apply to all

--- admin/editLetterBag.php
+++ admin/editLetterBag.php
@@ -6,6 +6,7 @@
     <link rel="icon" type="image/x-icon" href="favicon.ico"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://matcha.mizu.sh/matcha.css">
     <title>Edit Letter Bag - Scrabble Admin Panel</title>
 </head>
 <body>
