d71d0f0dc670aeccec9abd5a8e7ab873cd539c5d
colebot17 <colebot17@gmail.com>
2024-06-27 15:24:35 -0400

    Add the core nudge functionality

---
+++ php/nudge.php
@@ -0,0 +1,77 @@
+<?php
+
+// define connection
+$servername = "173.201.180.187";
+$username = "Colebot";
+$password = "96819822";
+$dbname = "scrabble";
+
+// create and check connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// get data from POST
+$user = $_POST['user'];
+$pwd = $_POST['pwd'];
+$gameId = $_POST['gameId'];
+
+// check password
+require "verifyPassword.php";
+if (!verifyPassword($conn, $user, $pwd)) {
+	exit('{"errorLevel":2,"message":"Invalid Session"}');
+}
+
+// make sure that the current player has a notification method
+$sql = "SELECT players, turn, name FROM games WHERE id='$gameId'";
+$query = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($query);
+if (!$row) exit(json_encode(Array("errorLevel" => 2, "message" => "Invalid game id.")));
+$players = json_decode($row['players'], true);
+$totalTurn = $row['turn'];
+$turn = $totalTurn % count($players);
+$gameName = $row['name'];
+
+$currentPlayerId = $players[$turn]['id'];
+$sql = "SELECT notificationMethods FROM accounts WHERE id='$currentPlayerId'";
+$query = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($query);
+$methods = json_decode($row['notificationMethods'], true);
+
+if (count($methods) === 0) {
+    exit(json_encode(
+        Array(
+            "errorLevel" => 1,
+            "message" => "This player cannot be nudged."
+        )
+    ));
+}
+
+
+// gather information
+$un = "";
+$playerList = Array();
+for ($i = 0; $i < count($players); $i++) {
+	$pid = $players[$i]['id'];
+	$sql = "SELECT name FROM accounts WHERE id='$pid'";
+	$query = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($query);
+	$playerList[] = $row['name'];
+	if ($pid === $user) $un = $row['name'];
+}
+
+
+// send a nudge notification to the player
+require "notifications/notify.php";
+require "notifications/templates/nudgeEmail.php";
+
+[$emailSubject, $emailBody] = nudgeEmail($un, $playerList[$turn], $gameName, $gameId, $playerList);
+notifyByEmail($conn, $currentPlayerId, $emailSubject, $emailBody);
+
+mysqli_close($conn);
+
+echo json_encode(Array(
+    "errorLevel" => 0,
+    "message" => "You nudged $playerList[$turn] to make their move"
+));
---
+++ scripts/nudge.js
@@ -0,0 +1,14 @@
+function nudge() {
+    request('nudge.php', {
+        user: account.id,
+        pwd: account.pwd,
+        gameId: game.id,
+    }).then(res => {
+        if (res.errorLevel > 0) {
+            textModal("Error", res.message);
+            return;
+        }
+
+        textModal("Success", res.message);
+    })
+}
