bb772c1030af177214779cb6e657cf52545b43f9
colebot17 <colebot17@gmail.com>
2024-06-30 14:16:42 -0400

    Don't adjust ctx scale

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -127,8 +127,6 @@ function setCanvasSize() {
 	// size the canvas accordingly
 	canvas.c.width = (limitingDimension - sizeDifference) * BOARD_PIXEL_SCALE;
 	canvas.c.height = limitingDimension * BOARD_PIXEL_SCALE;
-	canvas.ctx.setTransform(1, 0, 0, 1, 0, 0);
-	canvas.ctx.scale(1 / BOARD_PIXEL_SCALE, 1 / BOARD_PIXEL_SCALE);
 
 	// show the canvas again
 	canvas.c.style.display = "";
