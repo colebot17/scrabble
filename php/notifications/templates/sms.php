e66a3d25f32d0e72a8b96f860220702afb47a73d
colebot17 <colebot17@gmail.com>
2024-07-06 13:48:37 -0400

    Fix typo

--- php/notifications/templates/sms.php
+++ php/notifications/templates/sms.php
@@ -2,11 +2,11 @@
 
 $smsTemplates = Array(
     "friendRequest" => function ($fromName) {
-        $body = $fromName + ' wants to be your friend on scrabble.colebot.com. Log in to accept!';
+        $body = $fromName . ' wants to be your friend on scrabble.colebot.com. Log in to accept!';
         return $body;
     },
     "newGame" => function ($gameName, $gameId, $playerNames) {
-        $body = $playerNames[0] + ' created a game on scrabble.colebot.com with you! Log in to play.';
+        $body = $playerNames[0] . ' created a game on scrabble.colebot.com with you! Log in to play.';
         return $body;
     },
     "nudge" => function ($nudgingPlayerName, $gameName, $gameId, $playerNames) {
