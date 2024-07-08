d29dd6a9965af3563aac4d45cd1c7d9703a798ac
colebot17 <colebot17@gmail.com>
2024-07-08 11:11:11 -0400

    Change points to round

--- scripts/pointsChart.js
+++ scripts/pointsChart.js
@@ -51,7 +51,7 @@ function startChart() {
                 },
                 x: {
                     title: {
-                        text: "Turn",
+                        text: "Round",
                         display: true
                     },
                     grid: {
