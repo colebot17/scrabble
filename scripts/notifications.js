98dfb58697d9dbefdbe56248606851a96083cfd3
colebot17 <colebot17@gmail.com>
2024-07-15 13:12:19 -0400

    Clear cach

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -168,7 +168,7 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
     btn.disabled = false;
     btn.title = "";
     
-    navigator.serviceWorker.register("pushWorker.js");
+    navigator.serviceWorker.register("pushWorker.js?v=1");
 
     btn.addEventListener('click', () => {
         Notification.requestPermission().then(res => {
