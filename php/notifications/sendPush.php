08fc69b3202e616a5d1811ff43785add91da7fe1
colebot17 <colebot17@gmail.com>
2024-07-15 16:11:19 -0400

    Use other keys

--- php/notifications/sendPush.php
+++ php/notifications/sendPush.php
@@ -9,11 +9,19 @@ function sendPush($sub) {
 
     $subscription = Subscription::create($sub);
 
+    // $auth = Array(
+    //     "VAPID" => Array(
+    //         "subject" => "mailto: <colebot17@gmail.com>",
+    //         "publicKey" => "BDFxOE30BWtMOXpSGFdcTY5GrhGeI4EZZJG-TOVnK56J5Ehg-UTTevPDsuZ5owHVYYgBV_A8pdHFc-cDrhQWyFU",
+    //         "privateKey" => "JBne33dUaLrXJ21haGbOAU5quqoJsaSsdvs-kfEjlv4"
+    //     )
+    // );
+
     $auth = Array(
         "VAPID" => Array(
             "subject" => "mailto: <colebot17@gmail.com>",
-            "publicKey" => "BDFxOE30BWtMOXpSGFdcTY5GrhGeI4EZZJG-TOVnK56J5Ehg-UTTevPDsuZ5owHVYYgBV_A8pdHFc-cDrhQWyFU",
-            "privateKey" => "JBne33dUaLrXJ21haGbOAU5quqoJsaSsdvs-kfEjlv4"
+            "publicKey" => "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExGdM+I45xPZYDrCa5hW7B37PqZ3PufvbPEYRd0arAuo/3Er0dwzR3y6QdqZaTiN1zFn1tLIV73Azf0QSjPWoMw==",
+            "privateKey" => "MHcCAQEEIGDT6XhzkztiAzoT6+uyguNi7ejpHaSwS88LexCUj/+loAoGCCqGSM49AwEHoUQDQgAExGdM+I45xPZYDrCa5hW7B37PqZ3PufvbPEYRd0arAuo/3Er0dwzR3y6QdqZaTiN1zFn1tLIV73Azf0QSjPWoMw=="
         )
     );
 
