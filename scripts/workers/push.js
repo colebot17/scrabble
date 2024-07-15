ad773b09d599cdc36a530fc6edd539116c182a95
colebot17 <colebot17@gmail.com>
2024-07-15 10:59:39 -0400

    Handle the push event

---
+++ scripts/workers/push.js
@@ -0,0 +1,12 @@
+function receiveNotification(e) {
+    const text = e.data.text();
+    const title = "Yay! It is working!";
+    const options = {
+        data: "https://scrabble.colebot.com",
+        body: text
+    };
+
+    e.waitUntil(self.registration.showNotification(title, options));
+}
+
+self.addEventListener('push', receiveNotification);
