2384c41ef943294190d6836fb76dc587e351e244
colebot17 <colebot17@gmail.com>
2024-07-16 12:12:22 -0400

    Make text smaller

--- admin/testNotify.php
+++ admin/testNotify.php
@@ -15,7 +15,7 @@
     <?php
 
     if (!array_key_exists('user', $_GET)) {
-        exit('<h1 style="color:red">No Player Supplied</h1>');
+        exit('<h2 style="color:red">No Player Supplied</h2>');
     }
     $user = $_GET['user'];
 
