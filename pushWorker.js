14f7f237103c931554dccd48686e2c103dce4086
colebot17 <colebot17@gmail.com>
2024-07-16 01:29:31 -0400

    Use data

--- pushWorker.js
+++ pushWorker.js
@@ -2,7 +2,9 @@ function receiveNotification(e) {
     const msg = JSON.parse(e.data.text());
     const title = msg.title;
     const options = {
-        data: "https://scrabble.colebot.com",
+        data: {
+            game: 649
+        },
         body: msg.text
     };
 
@@ -14,7 +16,7 @@ self.addEventListener('push', receiveNotification);
 
 function notifClick(e) {
     e.preventDefault();
-    self.clients.openWindow('https://scrabble.colebot.com?game=649');
+    self.clients.openWindow('https://scrabble.colebot.com?game=' + e.notification.data.game);
 }
 
 self.addEventListener('notificationclick', notifClick);
