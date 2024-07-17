e7be7e5a765faec3fa1a7e19d9b3cc44b39612e2
colebot17 <colebot17@gmail.com>
2024-07-17 17:58:09 -0400

    Decode the message before sending notification

--- php/chat/sendChatMessage.php
+++ php/chat/sendChatMessage.php
@@ -26,7 +26,7 @@ if (!verifyPassword($conn, $user, $pwd)) {
 
 // trim the message
 $message = trim($message);
-$messageCopy = $message;
+$notifMessage = decodeURIComponent($message);
 $message = htmlentities($message);
 $message = str_replace(PHP_EOL, "<br>", $message);
 
@@ -58,7 +58,7 @@ $query = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($query);
 $un = $row['name'];
 
-$data = Array($un, $messageCopy, $gameName, $gameId);
+$data = Array($un, $notifMessage, $gameName, $gameId);
 require "../notifications/notify.php";
 
 for ($i = 0; $i < count($players); $i++) {
