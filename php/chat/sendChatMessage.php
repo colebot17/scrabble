eb7c6c0c64e9e2356cb1838ef4598e29e48aac40
colebot17 <colebot17@gmail.com>
2024-07-17 00:09:26 -0400

    Send copy of message before escaping

--- php/chat/sendChatMessage.php
+++ php/chat/sendChatMessage.php
@@ -26,6 +26,7 @@ if (!verifyPassword($conn, $user, $pwd)) {
 
 // trim the message
 $message = trim($message);
+$messageCopy = $message;
 $message = htmlentities($message);
 $message = str_replace(PHP_EOL, "<br>", $message);
 
@@ -57,7 +58,7 @@ $query = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($query);
 $un = $row['name'];
 
-$data = Array($un, $message, $gameName, $gameId);
+$data = Array($un, $messageCopy, $gameName, $gameId);
 require "../notifications/notify.php";
 
 for ($i = 0; $i < count($players); $i++) {
