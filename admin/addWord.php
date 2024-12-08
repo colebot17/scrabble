4b0d6b3e8b6b4780cccaaefb256e5aa1059816e5
colebot17 <colebot17@gmail.com>
2024-12-08 15:49:19 -0600

    Fix titles

--- admin/addWord.php
+++ admin/addWord.php
@@ -6,11 +6,11 @@
     <link rel="icon" type="image/x-icon" href="favicon.ico"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Word Lookup - Scrabble Admin Panel</title>
+    <title>Add Word - Scrabble Admin Panel</title>
 </head>
 <body>
     <h3>This is the admin panel for scrabble.colebot.com<br><a href="index.php">Admin Home</a></h3>
-    <h1>Word Lookup</h1>
+    <h1>Add Word</h1>
     <?php
 
     $word = strtolower($_GET['word']);
