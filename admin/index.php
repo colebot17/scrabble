0b0c452cb8e32f4982252e184518fa24defde27f
colebot17 <colebot17@gmail.com>
2024-12-08 13:16:17 -0600

    Add a way to lookup words from the admin panel

--- admin/index.php
+++ admin/index.php
@@ -22,6 +22,16 @@
     </form>
     <h3>User List</h3>
     <a href="userList.php">View All Users</a>
+    <h3>Is it a word?</h3>
+    <form action="wordLookup.php">
+        <input type="text" name="word" placeholder="Word" required>
+        <select name="language">
+            <option value="english">English</option>
+            <option value="spanish">Spanish</option>
+            <option value="french">French</option>
+        </select>
+        <button>Word Lookup</button>
+    </form>
     <h3>Invalidate Credentials</h3>
     <form action="validate.php" method="POST">
         <input type="hidden" name="invalidate" value="true">
---
+++ admin/wordLookup.php
@@ -0,0 +1,33 @@
+<?php if (!array_key_exists('password', $_COOKIE) || $_COOKIE['password'] !== '96819822') header('Location: validate.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Word Lookup - Scrabble Admin Panel</title>
+</head>
+<body>
+    <h3>This is the admin panel for scrabble.colebot.com<br><a href="index.php">Admin Home</a></h3>
+    <h1>Word Lookup</h1>
+    <?php
+
+    $word = $_GET['word'];
+    if (!$word) header('Location: index.php');
+
+    $language = $_GET['language'] || "english";
+
+    $dictFile = file_get_contents("../resources/dictionary_" . $language);
+
+    if (!$dictFile) header('Location: index.php');
+
+    $dictionary = json_decode($dictFile)["words"];
+
+    $isWord = in_array(strtolower($word), $dictionary);
+
+    echo "<h2 style='color:" . ($isWord ? "green" : "red") . "'>'$word' " . ($isWord ? "is" : "is not") . " a word</h2>";
+
+    ?>
+</body>
+</html>
