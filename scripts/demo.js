a7344dada8973bb66fb2e4348758d0056ce0edfb
colebot17 <colebot17@gmail.com>
2025-01-05 17:43:30 -0500

    Smartly determine what game to load

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,6 +1,24 @@
 async function demo() {
     await signIn("Demo Account", "", false);
-    loadGame(674);
+    for (let i = 0; i < account.games.length; i++) {
+        const g = account.games[i];
+
+        const isUsersTurn = parseInt(g.players[parseInt(g.turn) % g.players.length].id) === account.id
+
+        if (isUsersTurn && !g.inactive) {
+            loadGame(g.id);
+            return;
+        }
+    }
+
+    for (let i = 0; i < account.games.length; i++) {
+        const g = account.games[i];
+
+        if (!g.inactive) {
+            loadGame(g.id);
+            return;
+        }
+    }
 }
 
 document.getElementById('demoButton').addEventListener('click', demo);
