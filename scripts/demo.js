274651cba3f5385efc3c04a61d211e098c52a6e6
colebot17 <colebot17@gmail.com>
2025-01-05 18:37:44 -0500

    Select random game (within parameters)

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,23 +1,28 @@
 async function demo() {
     await signIn("Demo Account", "", false, true);
+
+    let cts = [];
     for (let i = 0; i < account.games.length; i++) {
         const g = account.games[i];
-
         const isUsersTurn = parseInt(g.players[parseInt(g.turn) % g.players.length].id) === account.id
-
-        if (isUsersTurn && !g.inactive) {
-            loadGame(g.id);
-            return;
-        }
+        
+        if (!g.inactive && isUsersTurn) cts.push(g);
+    }
+    if (cts.length > 0) {
+        let randId = cts[Math.floor(Math.random() * cts.length)].id;
+        loadGame(randId);
+        return;
     }
 
+    let actives = [];
     for (let i = 0; i < account.games.length; i++) {
         const g = account.games[i];
-
-        if (!g.inactive) {
-            loadGame(g.id);
-            return;
-        }
+        if (!g.inactive) actives.push(g);
+    }
+    if (actives.length > 0) {
+        let randId = actives[Math.floor(Math.random() * cts.length)].id;
+        loadGame(randId);
+        return;
     }
 }
 
