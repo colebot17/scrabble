c9133af37f5728a34d8dace8e8a4417e11ab9325
colebot17 <colebot17@gmail.com>
2025-01-07 14:31:19 -0500

    Require_once

--- php/notifications/sendEmail.php
+++ php/notifications/sendEmail.php
@@ -8,7 +8,7 @@ require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
-require '/../../vendor/autoload.php';
+require_once(__DIR__ . '/../../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
--- php/notifications/sendPush.php
+++ php/notifications/sendPush.php
@@ -17,7 +17,7 @@ function sendPush($sub, $messageObj) {
     //     )
     // );
 
-    require '/../../vendor/autoload.php';
+    require_once(__DIR__ . '/../../vendor/autoload.php');
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
