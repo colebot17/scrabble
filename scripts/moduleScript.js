7f5eda4d963bbc750ef630f1c96c18e98a33e35b
colebot17 <colebot17@gmail.com>
2025-01-30 23:11:52 -0500

    Use https for cors request

--- scripts/moduleScript.js
+++ scripts/moduleScript.js
@@ -3,7 +3,7 @@
 import { gameBanner as gameBanner_ } from "./gameBanner.js";
 window.gameBanner = gameBanner_;
 
-import { averageColor as averageColor_, multiplyColor as multiplyColor_ } from "//www.colebot.com/colors.js";
+import { averageColor as averageColor_, multiplyColor as multiplyColor_ } from "https://www.colebot.com/colors.js";
 window.averageColor = averageColor_;
 window.multiplyColor = multiplyColor_;
 
